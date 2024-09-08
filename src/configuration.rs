2e574f7d1c083b8f32a737f5bae67d6854ce0d60
ZerataX <mail@zera.tax>
2024-09-08 20:58:31 +0000

    remove obvious comments

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,7 +5,7 @@ use std::str::FromStr;
 #[derive(Clone, Debug, Deserialize)]
 pub struct Config {
     #[serde(deserialize_with = "deserialize_fixed_offset")]
-    pub timezone: FixedOffset, // Now we will manually deserialize this field
+    pub timezone: FixedOffset,
     pub discord_token: String,
     pub recipient_email: String,
     pub sender_email: String,
@@ -14,11 +14,10 @@ pub struct Config {
     pub smtp_username: String,
 }
 
-// Custom deserialization function for FixedOffset
 fn deserialize_fixed_offset<'de, D>(deserializer: D) -> Result<FixedOffset, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let s: String = Deserialize::deserialize(deserializer)?; // Deserialize the string first
-    FixedOffset::from_str(&s).map_err(serde::de::Error::custom) // Try to parse it using FixedOffset::from_str
+    let s: String = Deserialize::deserialize(deserializer)?;
+    FixedOffset::from_str(&s).map_err(serde::de::Error::custom)
 }
